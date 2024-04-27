5fb659f14fb16fe2750f5ff24fb882d5a17a7077
43hk <3408796042@qq.com>
2024-04-28 00:25:11 +0800

    提交代码

--- include/shader.h
+++ include/shader.h
@@ -4,17 +4,20 @@
 const char *vertexShaderSource = 
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos, 1.0);\n"
+    "   ourColor = aColor;\n"
     "}\0";
 
 //片段着色器
 const char *fragmentShaderSource =
     "#version 330 core\n"
+    "in vec3 ourColor;\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = ourColor;\n"
+    "   FragColor = vec4(ourColor, 1.0);\n"
     "}\0";
